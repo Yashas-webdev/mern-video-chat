4b7dc142493f506b03bd00bdce4c0cc6072fdf53
Yashas-webdev <yashasaryashu@gmail.com>
2026-03-14 00:38:21 +0530

    initial setup: folder structure,server.js, db.js, env config

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`);
+});
