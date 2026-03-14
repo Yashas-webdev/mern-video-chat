823a48f9ce3cd6f44f533a8b7f6b52d384ad6b31
Yashas-webdev <yashasaryashu@gmail.com>
2026-03-14 20:00:30 +0530

    feat: add auth middleware for protected routes

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken'); //to verify the token sent by the client.
+const User = require('../models/User'); //After verifying the token we fetch the actual user from MongoDB and attach it to the request.
+
+const protect = async( requestAnimationFrame, res, next) => {
+    let token;
+
+    if (req.headers.authorization && 
+        req.header.authorization.startsWith('Bearer')
+    ){
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select('-password');
+            next();
+        } catch (error){
+            return res.status(401).json({message: 'Not authorized, token fialed'});
+        }
+    }
+
+    if(!token){
+        return res.status(401).json({message:'Not authorized, no token'});
+    }
+};
+
+module.exports = {protect}
