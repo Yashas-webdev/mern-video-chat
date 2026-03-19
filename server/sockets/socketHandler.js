00fe85b9c8320f78b025355fab0816037baed29e
Yashas-webdev <yashasaryashu@gmail.com>
2026-03-19 21:33:50 +0530

    feat: added to fucntion which updates newly joined about the members already in the room and users about newly joined

--- server/sockets/socketHandler.js
+++ server/sockets/socketHandler.js
@@ -4,6 +4,25 @@ const socketHandler = (io) => {
         console.log(`User connected: ${socket.id}`)
         socket.on('join-room',({roomId, username})=>{
             socket.join(roomId)
+
+            if(!rooms.has(roomId)){
+                rooms.set(roomId,[])
+            }
+            rooms.get(roomId).push({socketId: socket.id, username})
+
+            const usersInRoom = rooms.get(roomId).filter(
+                user => user.socketId != socket.id
+            )
+
+            socket.emit('users-in-room', usersInRoom)
+
+            socket.to(roomId).emit('user-joined',{
+                socketId : socket.id,
+                username
+            })
+
+            console.log(`${username} joined room: ${roomId}`               
+            )
         })
     })
 }
