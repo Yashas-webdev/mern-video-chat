b6f607f87d169a71ddd5cdbefafd36bd1dfbe603
Yashas-webdev <yashasaryashu@gmail.com>
2026-03-19 21:52:52 +0530

    feat: complete sockethandler with WebRTC signaling andisconnect handling

--- server/sockets/socketHandler.js
+++ server/sockets/socketHandler.js
@@ -24,5 +24,45 @@ const socketHandler = (io) => {
             console.log(`${username} joined room: ${roomId}`               
             )
         })
+
+        socket.on('offer',({offer, to})=>{
+            socket.to(to).emit('offer',{
+                offer,
+                from: socket.id
+            })
+        })
+
+        socket.on('answer',({answer,to})=>{
+            socket.to(to).emit('answer',{
+                answer,
+                from: socket.id
+            })
+        })
+
+        socket.on('ice-candidate', ({candidate, to})=>{
+            socket.to(to).emit('ice-candidate',{
+                candidate,
+                from: socket.id
+            })
+        })
+
+        socket.on('disconnect',()=>{
+            console.log(`User disconnected: ${socket.id}`)
+            rooms.forEach((users, roomId)=>{
+                const index = users.findIndex(u => u.socketId === socket.id)
+                if(index !== -1){
+                    const [removedUser] = users.splice(index, 1)
+                    if(users.length === 0){
+                        rooms.delete(roomId)
+                    }
+                    io.to(roomId).emit('user-left',{
+                        socketId: socket.id,
+                        username: removedUser.username
+                    })
+                }
+            })
+        })
     })
 }
+
+Module.exports = socketHandler
