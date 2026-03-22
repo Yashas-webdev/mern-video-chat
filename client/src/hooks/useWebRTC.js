87770f134eec4857d5a773d8984461335b977194
Yashas-webdev <yashasaryashu@gmail.com>
2026-03-23 00:37:10 +0530

    feat: add useWEbRTC hook - peer connection and room initialization

--- client/src/hooks/useWebRTC.js
+++ client/src/hooks/useWebRTC.js
@@ -33,4 +33,47 @@ const useWebRTC = (socket, roomId, username) =>{
         }
         
     },[])
+
+    const createPeerConnection = useCallback((socketId)=>{
+        const pc = new RTCPeerConnection(ICE_SERVERS)  // Creates a brand new WebRTC connection using the browser's built-in `RTCPeerConnection` API. We pass our STUN servers config so WebRTC knows how to find public IP addresses.
+    
+
+    pc.onicecandidate = (event) => { //This is an event handler that fires automatically every time WebRTC finds a new network path (ICE candidate) to reach the other person.
+
+        if(event.candidate && socket){
+            socket.emit('ice-candidate',{
+                candidate:event.candidate,
+                to:socketId
+            })
+        }
+    }
+
+    pc.ontrack = (event) => {
+        setRemoteStreams(prev => {
+            const exists = prev.find(s => s.socketId === socketId)
+            if(exists) return prev
+            return [...prev, {socketId, stream:event.streams[0]}]
+        })
+    }
+
+    if(localStreamRef.current){
+        localStreamRef.current.getTracks().forEach(track => {
+            pc.addTrack(track, localStreamRef.current)
+        })
+    }
+
+    peerConnectionsRef.current[socketId] = pc
+    return pc
+    },[socket])
+
+
+    useEffect(()=>{
+        if(!socket || !roomId) return 
+
+        const initializeRoom = async() => {
+            await getLocalStream()
+            socket.emit('join-room',{roomId, username})
+        }
+        initializeRoom()
+    })
 }
