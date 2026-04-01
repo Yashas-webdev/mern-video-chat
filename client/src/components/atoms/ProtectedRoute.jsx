a1bc680146a17c2e3c966cc9e35ddbacfd0c8937
Yashas-webdev <yashasaryashu@gmail.com>
2026-04-01 23:05:56 +0530

    feat : ProtectedRoute created

--- client/src/components/atoms/ProtectedRoute.jsx
+++ client/src/components/atoms/ProtectedRoute.jsx
@@ -1,1 +1,10 @@
+import useAuthStore from '../../store/useAuthStore'
 import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({children}) => {
+    const user = useAuthStore(state => state.user)
+
+    if(!user){
+        return <Navigate to='/login' replace />
+    }
+}
