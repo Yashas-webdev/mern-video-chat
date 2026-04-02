f2e5ea2b2b1d5dad4be3eff04ba2d55683694c32
Yashas-webdev <yashasaryashu@gmail.com>
2026-04-02 21:18:17 +0530

    feat : Add protected routes(redirect to login if not logged in)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,21 @@ import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
 import RoomPage from './pages/RoomPage'
+import ProtectedRoute from './components/atoms/ProtectedRoute'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div><HomePage/></div>} />
+        <Route path="/" element={
+          <ProtectedRoute><HomePage/></ProtectedRoute>
+          } />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/room/:roomId" element={<RoomPage/>} />
+        <Route path="/room/:roomId" element={
+          <ProtectedRoute>
+            <RoomPage/>
+          </ProtectedRoute>} />
       </Routes>
     </Router>
   )
