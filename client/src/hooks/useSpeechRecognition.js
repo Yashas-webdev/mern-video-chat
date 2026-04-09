6cb50cc4e2c7779f9e0cf06a41b4e4e391b22756
Yashas-webdev <yashasaryashu@gmail.com>
2026-04-09 09:18:23 +0530

    feat: add speech recognition hook and transcript server evens

---
+++ client/src/hooks/useSpeechRecognition.js
@@ -0,0 +1,42 @@
+import { useState, useEffect, useRef} from 'react'
+
+const useSpeechRecognition = (socket, roomId, username) =>{
+    const recognitioReft = useRef(null)
+    const [isListening, setIsListening] = useState(false)
+    const [localTranscript, setLocalTranscript] = useState([])
+
+    useEffect(()=>{
+        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+        if(!SpeechRecognition){
+            console.log("Speech recognition not supported in this browser");
+            return
+        }
+
+        const recognition = new SpeechRecognition()
+        recognition.continous = true,
+        recognition.interimResults = false,
+        recognition.lang = 'en-Us'
+
+        recognition.onresult = (event) =>{
+            const lastResult = event.results[event.results.length - 1]
+            const text = lastResult[0].transcript.trim()
+            if(text && socket){
+                const entry = {
+                    username, text, timestamp: Date.now()}
+                    setLocalTranscript(prev => [...prev, entry])
+                    socket.emit('transcript-update',{roomId, username, text})
+            }
+        }
+
+        recognition.onerror = (event) => {
+            console.log('Speech recognition error:',event.error)
+            setIsListening(false)
+        }
+
+        recognitioReft.current = recognition
+
+        return () => {
+            recognition.stop()
+        }
+    },[socket,roomId,username])
+}
