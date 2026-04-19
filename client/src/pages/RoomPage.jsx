0c581d27b49a9931598c37ee5db32e381025a11b
Yashas-webdev <yashasaryashu@gmail.com>
2026-04-19 23:40:47 +0530

    resolved the error in the SVG path Transcription button

--- client/src/pages/RoomPage.jsx
+++ client/src/pages/RoomPage.jsx
@@ -283,7 +283,7 @@ const RoomPage = () => {
           }`}  
         >
           <svg className='w-5 h-5 text-white' fill='none' stroke='currentColor' viewBox='0 0 24 24'>
-            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.589a1 1 0 01.707.29315.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z "/>
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"/>
           </svg>
           <span className='text-white text-xs'>
             {isListening ? 'Stop Notes' : 'Take Notes'}
